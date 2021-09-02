7114167e5459b9ad4c7a239177c2c7fc8d720950
Adam Herro <adam.herro@comcast.net>
2021-09-02 19:47:57 -0400

    Adding the css-loader and adding ability to add a To-Do from the input field and display on the screen.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import loadPage from "./modules/UI"
+
+document.addEventListener('DOMContentLoaded', loadPage)
+
+
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,12 +1,47 @@
-import ToDo from "./modules/to_do";
+import './CSS/style.css'
+import ToDo from "./to_do";
 import Project from "./projects";
 
-function createToDo(item) {
-    item = new ToDo;
-     
+
+function displayToDoInput() {
+    const openInputBtn = document.querySelector('.openToDoInput')
+    openInputBtn.addEventListener('click', () => {
+        const addSection = document.querySelector('.addToDoSection')
+        addSection.style.display = 'Block'
+    })
+}  
+
+
+function createToDo() {
+    let item = new ToDo
+    let nameInput = document.querySelector('.nameInput')
+    let descriptionInput = document.querySelector('.descriptionInput')
+    item.setName(nameInput.value)
+    item.setDescription(descriptionInput.value)
+
+    let display = document.querySelector('.toDoList');
+    let toDoItem =  document.createElement('div');
+    toDoItem.classList.add('toDoCard');
+    let name = document.createElement('li')
+    name.classList.add('name');
+    name.textContent = item.name
+    let description = document.createElement('li');
+    description.classList.add('description');
+    description.textContent = item.description
+    toDoItem.append(name)
+    toDoItem.append(description)
+    display.appendChild(toDoItem)
+}
+let submitBtn = document.querySelector('.addItemBtn')
+submitBtn.addEventListener('click', createToDo)
+
+function loadPage() {
+    displayToDoInput();
 }
 
+export default loadPage;
 
+/*
 let one = new ToDo;
 
 one.setName('Take out trash')
@@ -14,8 +49,10 @@ one.setDescription('I need to take out the trash so it does not smell.')
 console.log(one.name)
 console.log(one.description)
 
+
 let body = document.querySelector('body')
 let title = document.createElement('h1')
 title.append(one.name)
 title.append(one.description)
 body.append(title)
+*/

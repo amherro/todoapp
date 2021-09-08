8110410da9c6000b67254bf17b7aa4711646a21a
Adam Herro <adam.herro@comcast.net>
2021-09-08 16:01:33 -0400

    Adding CSS to style app.

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -12,10 +12,15 @@ export default class CreateList {
         let submitBtn = document.querySelector('.submitTask')
         submitBtn.addEventListener('click', () => {
             let task = new ToDo;
+            /*
+            let activateForm = document.querySelector('.activateForm')
+            activateForm.addEventListener('click', () => {
+                let addTaskForm = document.querySelector('.addTaskForm')
+                addTaskForm.style.display = 'flex'
+            })\
+            */
             let taskName = document.querySelector('.taskName')
-            let taskDescription = document.querySelector('.description')
             task.setName(taskName.value)
-            task.setDescription(taskDescription.value)
 
             let taskList = document.querySelector('.taskList');
             let taskCard = document.createElement('div')
@@ -23,23 +28,15 @@ export default class CreateList {
             let name = document.createElement('li')
             name.classList.add('name')
             name.textContent = task.name;
-            let description = document.createElement('li')
-            description.classList.add('description')
-            if (task.description === '') {
-                description.style.display = 'None'
-            } else {
-                description.textContent = task.description
-            }
 
             let deleteBtn = document.createElement('button')
             deleteBtn.classList.add('deleteBtn')
-            deleteBtn.textContent = '-'
+            deleteBtn.textContent = 'X'
             deleteBtn.addEventListener('click', () => {
                 taskCard.style.display = 'None'
             })
 
             taskCard.append(name)
-            taskCard.append(description)
             taskCard.append(deleteBtn)
             taskList.append(taskCard)
 
@@ -50,102 +47,3 @@ export default class CreateList {
 }
 let newTask = new CreateList
 newTask.addTask();
-
-/*
-const createList = () => {
-    let taskDisplay = document.querySelector('.taskDisplay')
-    let project = new Project;
-    let projectsArr = [];
-    function addProject() {
-        const projectNameInput = document.querySelector('.projectName')
-        project.setProjectName(projectNameInput.value)
-        projectsArr.push(project.title)
-        //console.log(projectsArr)
-        
-        let projectDisplay = document.querySelector('.projectList')
-        // Make each project a button, and when clicked it will display the associated to do's
-        let newProject = document.createElement('button')
-        newProject.classList.add('newProjectCard')
-        let projectTitle = document.createElement('h3')
-        projectTitle.textContent = project.title;
-        newProject.append(projectTitle);
-        projectDisplay.append(newProject);
-        projectNameInput.value = '';
-        
-        let displayTasks = () => {
-            newProject.addEventListener('click', () => {
-                function displayToDoInput() {
-                    let itemDisplay = document.createElement('main')
-                    itemDisplay.classList.add('itemDisplay')
-                    let toDoTitle = document.createElement('h2')
-                    toDoTitle.classList.add('toDoTitle')
-                    let toDoSection = document.createElement('div')
-                    toDoSection.classList.add('toDoSection');
-                    let inputTitle = document.createElement('h3')
-                    inputTitle.textContent = 'Add a New Item'
-                    let nameInput = document.createElement('input')
-                    nameInput.classList.add('nameInput')
-                    nameInput.type = 'text'
-                    let descriptionInput = document.createElement('input')
-                    descriptionInput.classList.add('descriptionInput')
-                    let addItemBtn = document.createElement('button')
-                    addItemBtn.classList.add('addItemBtn')
-                    addItemBtn.textContent = '+'
-                    itemDisplay.append(toDoTitle);
-                    itemDisplay.append(toDoSection);
-                    itemDisplay.append(inputTitle);
-                    itemDisplay.append(nameInput);
-                    itemDisplay.append(descriptionInput);
-                    itemDisplay.append(addItemBtn);
-
-                    taskDisplay.append(itemDisplay)
-
-                    itemDisplay.classList.add(`${project.title.replace(/\s+/g, '')}`)
-                    console.log(itemDisplay.classList)
-                    if(itemDisplay.classList.contains(project.title.replace(/\s+/g, ''))) {
-                        itemDisplay.style.display = 'Block'
-                    }
-                    addItemBtn.addEventListener('click', () => {
-                        function createToDo() {
-                            let item = new ToDo
-                            let nameInput = document.querySelector('.nameInput')
-                            let descriptionInput = document.querySelector('.descriptionInput')
-                            item.setName(nameInput.value)
-                            item.setDescription(descriptionInput.value)
-                        
-                            let display = document.querySelector('.taskDisplay');
-                            let toDoItem =  document.createElement('div');
-                            toDoItem.classList.add('toDoCard');
-                            let name = document.createElement('li')
-                            name.classList.add('name');
-                            name.textContent = item.name
-                            let description = document.createElement('li');
-                            description.classList.add('description');
-                            description.textContent = item.description
-                            toDoItem.append(name)
-                            toDoItem.append(description)
-                            display.append(toDoItem)
-                            nameInput.textContent = '';
-                            descriptionInput.textContent = '';
-                        }
-                        let submitBtn = document.querySelector('.addItemBtn')
-                        submitBtn.addEventListener('click', createToDo);
-                    })
-                }
-                displayToDoInput();
-            })
-        }
-        displayTasks();
-    }
-    addProject()
-}
-
-let submitProjectBtn = document.querySelector('.addProjectBtn')
-submitProjectBtn.addEventListener('click', createList);
-
-function loadPage() {
-    
-}
-
-export default loadPage;
-*/
